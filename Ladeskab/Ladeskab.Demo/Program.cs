387fe75b57d14c6ad000c8ff6d826f35324dd5dd
nikolaj808 <45153292+nikolaj808@users.noreply.github.com>
2020-03-24 09:21:53 +0100

    Added Ladeskab demo

---
+++ Ladeskab/Ladeskab.Demo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsbChargerSimulator usb = new UsbChargerSimulator();
+            IChargeControl charger = new ChargeControl(usb);
+            IDoor door = new Door();
+            RFIDReader rfidReader = new RFIDReader();
+            IDisplay display = new ConcreteDisplay();
+            StationControl control = new StationControl(charger, door, display, rfidReader);
+
+            bool finish = false;
+            do
+            {
+                string input;
+                System.Console.WriteLine("Indtast E, O, C, R: ");
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) continue;
+
+                switch (input[0])
+                {
+                    case 'E':
+                        finish = true;
+                        break;
+
+                    case 'O':
+                        door.OpenDoor();
+                        break;
+
+                    case 'C':
+                        door.CloseDoor();
+                        break;
+
+                    case 'R':
+                        System.Console.WriteLine("Indtast RFID id: ");
+                        string idString = System.Console.ReadLine();
+
+                        int id = Convert.ToInt32(idString);
+                        rfidReader.ReadRFID(id);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } while (!finish);
+        }
+    }
+}
